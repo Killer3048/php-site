c5b9dca574c2be7478d42e4cfac3d13f13db7dbd
unknown <gamert11@mail.ru>
2023-01-30 22:46:09 +0700

    sd

--- lib/connect.php
+++ lib/connect.php
@@ -1,6 +1,6 @@
 <?php
 $db = mysqli_connect(
-    '127.0.0.1', "admin", "Killer-2005", "u1918010_data");
+    '127.0.0.1', "u1918010_admin", "Killer-2005", "u1918010_data");
     if(!$db){
     die('нет соединения'. mysqli_connect_error());
     };

fe108d64a35c66bb443b4acc606f6906d0016b6b
unknown <gamert11@mail.ru>
2023-02-01 22:31:52 +0700

    sd

--- static/script.js
+++ static/script.js
@@ -1,6 +1,46 @@
-const btn = document.querySelector('.form__btn');
-btn.addEventListener('click', function() {
-    btn.style.width = '40%';
-    btn.style.padding = '5px 10px';
-    btn.style.backgroundColor = '#6F6F6F';
+const btn = document.querySelector(".form__btn");
+const modal = document.getElementById("modal");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const gay = document.querySelector(".gay");
+let noClicked = false;
+
+btn.addEventListener("click", function () {
+  btn.style.width = "40%";
+  btn.style.padding = "5px 10px";
+  btn.style.backgroundColor = "#6F6F6F";
+});
+
+yesBtn.addEventListener("click", () => {
+  gay.style.display = "block";
+  modal.style.display = "none";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const randomX = Math.floor(
+    Math.random() * (window.innerWidth - modal.offsetWidth)
+  );
+  const randomY = Math.floor(
+    Math.random() * (window.innerHeight - modal.offsetHeight)
+  );
+  modal.style.top = `${randomY}px`;
+  modal.style.left = `${randomX}px`;
+});
+
+noBtn.addEventListener("click", () => {
+  if (!noClicked) {
+    const randomX = Math.floor(
+      Math.random() * (window.innerWidth - modal.offsetWidth)
+    );
+    const randomY = Math.floor(
+      Math.random() * (window.innerHeight - modal.offsetHeight)
+    );
+    modal.style.top = `${randomY}px`;
+    modal.style.left = `${randomX}px`;
+    noClicked = true;
+  }
+});
+
+noBtn.addEventListener("click", () => {
+  alert("Иди в попу");
 });
